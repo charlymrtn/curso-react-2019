52caee21898066e17c02b1a1db70e7947a274e11
carlos martin <cjmar87@gmail.com>
2019-04-02 16:05:31 -0600

    practica blog

---
+++ blog/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Blog from "./Blog";
+
+var destination = document.getElementById('container');
+
+ReactDOM.render(<Blog />, destination);
