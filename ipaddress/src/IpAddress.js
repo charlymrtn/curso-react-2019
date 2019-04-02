4caf92655361e38a765abba3d58207f0edb68c79
carlos martin <cjmar87@gmail.com>
2019-04-02 13:32:20 -0600

    lab 12 ip address

---
+++ ipaddress/src/IpAddress.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+//import "./HelloWorld.css";
+ 
+class IpAddress extends Component {
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+    
+export default IpAddress;
---
+++ ipaddress/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import IpAddress from "./IpAddress";
+import "./index.css";
+    
+ReactDOM.render(
+    <IpAddress/>,
+    document.getElementById("container")
+);
